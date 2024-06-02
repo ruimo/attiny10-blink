45b57240225e65b5006660ba5d5f4aa987a362ad
Shisei Hanai <ruimo.uno@gmail.com>
2024-06-02 13:41:22 +0900

    Dockerize

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-mkdocs serve -a localhost:8040
+export U_ID=$(id -u)
+export G_ID=$(id -g)
+docker run -it --rm \
+    -p 8040:8040 \
+    --user $U_ID:$G_ID \
+    --workdir="/var/home" \
+    --env HOME="/var/home" \
+    --volume="$PWD:/var/home" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" squidfunk/mkdocs-material serve -a 0.0.0.0:8040
+
